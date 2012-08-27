08c82072ca8409e5d019ddcc37da46c9d544d3e1
Javier Alvarez <jalvarezsamayoa@gmail.com>
2012-08-27 08:21:29 -0600

    fix assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,5 +71,4 @@ OpenwolfV4::Application.configure do
   #configuracion para metodo de envio de correos
   config.action_mailer.delivery_method = :sendmail
 
-  config.assets.initialize_on_precompile = false
 end
