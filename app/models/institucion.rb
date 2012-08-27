2d2fb0958a4811dae4d350e9f3eaad54a4ffc945
Javier Alvarez <jalvarezsamayoa@gmail.com>
2012-08-27 08:46:38 -0600

    fix

--- app/models/institucion.rb
+++ app/models/institucion.rb
@@ -100,35 +100,35 @@ class Institucion < ActiveRecord::Base
   end
 
   def solicitudes_por_estado(ano = Date.today.year)
-    estados = Estado.select('estados.nombre, count(solicitudes.estado_id) as total_solicitudes').joins(:solicitudes).where("solicitudes.anulada = ? and solicitudes.institucion_id = ? and extract(year from fecha_creacion) = ?",false,self.id,ano).group('estados.nombre')
+    estados = Estado.select('estados.nombre, count(solicitudes.estado_id) as total_solicitudes').joins(:solicitudes).where("solicitudes.anulada = ? and solicitudes.institucion_id = ? and extract(year from solicitudes.fecha_creacion) = ?",false,self.id,ano).group('estados.nombre')
     return estados
   end
 
   def solicitudes_por_via_solicitud(ano = Date.today.year)
-    estados = Via.select('vias.nombre, count(solicitudes.via_id) as total_solicitudes').joins(:solicitudes).where("solicitudes.anulada = ? and solicitudes.institucion_id = ? and extract(year from fecha_creacion) = ?",false,self.id,ano).group('vias.nombre')
+    estados = Via.select('vias.nombre, count(solicitudes.via_id) as total_solicitudes').joins(:solicitudes).where("solicitudes.anulada = ? and solicitudes.institucion_id = ? and extract(year from solicitudes.fecha_creacion) = ?",false,self.id,ano).group('vias.nombre')
     return estados
   end
 
 
 
   def solicitudes_por_ano
-    solicitudes =  Solicitud.find_by_sql("select extract(year from fecha_creacion) as ano,  count(solicitudes.id) as total_solicitudes, avg(solicitudes.tiempo_respuesta) as promedio_dias_respuesta  from solicitudes where institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null group by extract(year from fecha_creacion) order by extract(year from fecha_creacion) asc")
+    solicitudes =  Solicitud.find_by_sql("select extract(year from solicitudes.fecha_creacion) as ano,  count(solicitudes.id) as total_solicitudes, avg(solicitudes.tiempo_respuesta) as promedio_dias_respuesta  from solicitudes where institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null group by extract(year from solicitudes.fecha_creacion) order by extract(year from solicitudes.fecha_creacion) asc")
 
     return solicitudes
   end
 
   def solicitudes_por_mes_ano
-    solicitudes =  Solicitud.find_by_sql("select extract(year from fecha_creacion) as ano, extract(month from fecha_creacion) as mes,  count(solicitudes.id) as total_solicitudes, avg(solicitudes.tiempo_respuesta) as promedio_dias_respuesta from solicitudes where institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null group by extract(year from fecha_creacion), extract(month from fecha_creacion) order by extract(year from fecha_creacion) asc, extract(month from fecha_creacion) asc")
+    solicitudes =  Solicitud.find_by_sql("select extract(year from solicitudes.fecha_creacion) as ano, extract(month from solicitudes.fecha_creacion) as mes,  count(solicitudes.id) as total_solicitudes, avg(solicitudes.tiempo_respuesta) as promedio_dias_respuesta from solicitudes where institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null group by extract(year from solicitudes.fecha_creacion), extract(month from solicitudes.fecha_creacion) order by extract(year from solicitudes.fecha_creacion) asc, extract(month from solicitudes.fecha_creacion) asc")
     return solicitudes
   end
 
   def tiempo_respuesta_promedio(ano = Date.today.year)
-    tiempo = Solicitud.find_by_sql("select avg(solicitudes.tiempo_respuesta) as promedio from solicitudes where solicitudes.institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null and extract(year from fecha_creacion) = #{ano}")
+    tiempo = Solicitud.find_by_sql("select avg(solicitudes.tiempo_respuesta) as promedio from solicitudes where solicitudes.institucion_id = #{self.id} and anulada = #{false} and solicitudes.fecha_completada is not null and extract(year from solicitudes.fecha_creacion) = #{ano}")
     return tiempo[0].promedio.to_f.round()
   end
 
   def solicitudes_por_genero_ano
-    solicitudes = Solicitud.find_by_sql("select genero_id, extract(year from fecha_creacion) as ano,  count(solicitudes.id) as total_solicitudes from solicitudes where institucion_id = #{self.id} and anulada = #{false}  group by genero_id, extract(year from fecha_creacion)  order by genero_id, extract(year from fecha_creacion) desc")
+    solicitudes = Solicitud.find_by_sql("select genero_id, extract(year from solicitudes.fecha_creacion) as ano,  count(solicitudes.id) as total_solicitudes from solicitudes where institucion_id = #{self.id} and anulada = #{false}  group by genero_id, extract(year from solicitudes.fecha_creacion)  order by genero_id, extract(year from solicitudes.fecha_creacion) desc")
   end
 
 
